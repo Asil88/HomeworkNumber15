f7379042eca6ff5d2a85fdff8e7b79967cc24696
Anton Lisovski <lisovskijj.99@gmail.com>
2022-10-21 11:08:47 +0300

    task1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,6 +1,7 @@
 package task1;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,9 +10,10 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
+        int listSize = random.nextInt(10,20);
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < listSize; i++) {
             list.add(random.nextInt(1, 20));
         }
         System.out.println("Заданная коллекция list:\n"+list);
@@ -22,21 +24,21 @@ public class Main {
                 .map(x -> x * 2).sorted((x1, x2) -> Integer.compare(x1, x2))
                 .limit(4).collect(Collectors.toList());
 
-        Average<Integer> average = new Average<Integer>() {
+        Supplier average = new Supplier() {
             @Override
-            public Integer operation() {
+            public Object get() {
                 int sum = 0;
-                for (int i = 0; i < list.size(); i++) {
-                    sum = sum + list.get(i);
+                for (int i = 0; i < result.size(); i++) {
+                    sum = sum + result.get(i);
                 }
-                int result = sum / list.size();
-                return result;
+                double res =(double) sum  / result.size();
+                return res;
             }
         };
 
         System.out.println("Оставшееся кол-во значений после проверки на уникальность и фильтраци: " + result.size());
         System.out.println("Оставшиеся после проверок значения в коллекции: "+result);
-        System.out.println("Среднее арифметическое: " + average.operation());
+        System.out.println("Среднее арифметическое: " + average.get());
 
     }
 }
